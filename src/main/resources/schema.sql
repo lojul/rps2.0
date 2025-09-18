aeff9e02396b7be992999bbfcc66aacee25e1165
Julian Lau <julian_lau@hotmail.com>
2025-09-18 18:55:13 +0800

    Switch to PostgreSQL: replace H2, update datasource, convert schema to SERIAL; update Lombok; clean JPA deps; remove Docker compose

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-DROP TABLE IF EXISTS rps_plan_group;
-DROP TABLE IF EXISTS member;
+DROP TABLE IF EXISTS rps_plan_group CASCADE;
+DROP TABLE IF EXISTS member CASCADE;
 
 CREATE TABLE rps_plan_group (
-                                id INT AUTO_INCREMENT  PRIMARY KEY,
+                                id SERIAL PRIMARY KEY,
                                 plan_group VARCHAR(250) NOT NULL,
                                 plan_group_desc VARCHAR(250) NOT NULL
 );
 
 CREATE TABLE member (
-                                id INT AUTO_INCREMENT  PRIMARY KEY,
+                                id SERIAL PRIMARY KEY,
                                 first_name VARCHAR(250) NOT NULL,
                                 last_name VARCHAR(250) NOT NULL,
                                 address VARCHAR(250) NOT NULL
